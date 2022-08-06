817dd1fbdf387b58a9590717675718708cad95b0
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-08-06 10:47:40 -0500

    Add files via upload

---
+++ herencia/herencia/src/herencia/Main.java
@@ -0,0 +1,39 @@
+ package herencia;
+
+
+    public class Main {
+        public static void main(String[] args) {
+            class Persona {
+
+            }
+            System.out.println("Nombre,Edad,Telefono,Credito");
+            String Nombre;
+            int Edad;
+            int Telefono;
+        }
+
+        class Cliente extends Persona {
+        }
+
+        int Credito;
+
+        class Object extends Cliente {
+
+        }
+
+        String Nombre;
+        int Edad;
+        int Telefono;
+        int credito;
+    }
+
+
+        class Trabajador extends Persona {
+
+            String Salario;
+        }
+
+
+
+
+
---
+++ herencia/herencia/src/herencia/Persona.java
@@ -0,0 +1,4 @@
+package herencia;
+
+public class Persona {
+}
